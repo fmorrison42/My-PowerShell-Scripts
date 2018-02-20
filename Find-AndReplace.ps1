4c10a3b3ad5023ff9a58bd883ec662104ce30dfd
Frank Morrison <fmorrison42@gmail.com>
2018-02-20 12:20:29 -0600

    Add files via upload

---
+++ Find-AndReplace.ps1
@@ -0,0 +1,5 @@
+﻿(Get-Content C:\users\fmorrison\desktop\exam70-740Topics.txt) -replace ';', "`n" | Set-Content C:\users\fmorrison\desktop\exam70-740Topics.txt -Force
+
+# go through each line and look for ';'
+# when it finds one, replace it with a new line
+
